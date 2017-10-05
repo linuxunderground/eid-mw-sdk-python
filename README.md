90a63df620a290105303491a0fabf511dc563ab5
Vincent Hardy <vincent.hardy.be@gmail.com>
2017-10-05 19:29:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# eid-mw-sdk-python
+
+## Overview
+
+This project shows how to use Belgian eid-mw with Python.
+To do this, we will use the Python PKCS#11 Wrapper from Ludovic Rousseau.
+
+eid-mw-sdk-python is licensed under the terms of the [GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html).
+See the LICENSE file for the full text of the license.
+
+Any suggestions or improvements are welcome.  
+Send them to vincent.hardy.be@gmail.com  
+
+Homepage: https://github.com/linuxunderground/eid-mw-sdk-python
+
+
+## PyKCS11
+
+You will find PyKCS11 at https://pypi.python.org/pypi/pykcs11  
+  
+Gentoo users can easily install PyKCS11 with ebuild available at the
+[Linuxunderground overlay](https://github.com/linuxunderground/gentoo.overlay/tree/master/dev-python/PyKCS11).  
---
+++ eid-data.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 Vincent Hardy (vincent.hardy.be@gmail.com)
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version
+# 3.0 as published by the Free Software Foundation.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software; if not, see
+# https://www.gnu.org/licenses/
+#
+
+from PyKCS11 import *
+import platform
+import sys
+
+class getData(object):
+
+    def __init__(self):
+        self.pkcs11 = PyKCS11.PyKCS11Lib()
+        if platform.system().lower() != 'windows':
+            self.pkcs11.load('libbeidpkcs11.so')
+        else:
+            self.pkcs11.load('beidpkcs11.dll')
+
+    def getInfo(self):
+        print(self.pkcs11.getInfo())
+        print('======================================================')
+        print()
+
+    def getData(self, slot, name):
+        session = self.pkcs11.openSession(slot)
+        o = session.findObjects([(CKA_CLASS, CKO_DATA), (CKA_LABEL, name)])[0]        
+        value = session.getAttributeValue(o,[CKA_VALUE])[0]
+        #print(value)
+        text = bytes(value).decode('utf-8')
+        print(text)
+
+
+if __name__ == '__main__':
+
+    beid = getData()
+    beid.getInfo()
+
+    slots = beid.pkcs11.getSlotList()
+    
+    if len(slots) == 0:
+        sys.exit(2)
+
+    for slot in slots:
+        try:
+            beid.getData(slot, 'surname')
+            beid.getData(slot, 'firstnames')
+        except PyKCS11.PyKCS11Error as e:
+            print("Error:", e)
